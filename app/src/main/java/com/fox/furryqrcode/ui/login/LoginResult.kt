8424a4229178fe9bacb68a41fcec4d00b4ca6238
Xiao Shou <chenbendan2@163.com>
2020-05-24 08:59:38 +0800

    clean

---
+++ app/src/main/java/com/fox/furryqrcode/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.fox.furryqrcode.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
