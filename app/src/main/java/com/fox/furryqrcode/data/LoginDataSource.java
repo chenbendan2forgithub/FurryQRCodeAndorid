e3a9b9a9a9bea345e68634c4f6dfd767f80ce9c4
Xiao Shou <chenbendan2@163.com>
2020-05-24 14:49:26 +0800

    Clean Tree

---
+++ app/src/main/java/com/fox/furryqrcode/data/LoginDataSource.java
@@ -0,0 +1,29 @@
+package com.fox.furryqrcode.data;
+
+import com.fox.furryqrcode.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("登陆失败", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
