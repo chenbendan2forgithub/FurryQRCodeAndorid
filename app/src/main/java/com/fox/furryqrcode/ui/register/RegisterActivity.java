7fdc19cd153952ce39ce3a44f101158ba82c2b1e
Xiao Shou <chenbendan2@163.com>
2020-05-24 14:48:34 +0800

    Clean Tree

---
+++ app/src/main/java/com/fox/furryqrcode/ui/register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.fox.furryqrcode.ui.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fox.furryqrcode.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
